69921a4421efe1dde6fa12b6a4653f97aa7717f8
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-04-01 11:42:55 +1100

    set up uploader to save gps data

--- readingdb/uploader.py
+++ readingdb/uploader.py
@@ -1,6 +1,18 @@
 import boto3
+import json
+import random
+import string
 
+from datetime import datetime
+from readingdb.api import API
 class Uploader():
+    @staticmethod
+    def load_json_entries(path_key, reading):
+        with open(reading[path_key], "r") as f:
+            entries = json.load(f)
+        
+        return entries
+
     AUTH_RESULT_KEY = "AuthenticationResult"
     ACCESS_TOKEN_KEY = "AccessToken"
     USER_ATTR_KEY = "UserAttributes"
@@ -9,8 +21,27 @@ class Uploader():
     USERNAME_KEY = "Username"
 
     USER_SUB = 'sub'
+    
+    ROUTE_NAME_KEY = "route"
+    ROUTE_READINGS_KEY = "readings"
+    READING_TYPE_KEY = "type"
+    READING_FORMAT_KEY = "format"
+    READING_PATH_KEY = "path"
 
-    def __init__(self, auth, region_name="ap-southeast-2"):
+    GPS_TYPE = "gps"
+    CAMERA_TYPE = "camera"
+    JSON_ENTRIES_FORMAT = "json_entries"
+
+    RECOGNIZED_READING_TYPES = {
+        CAMERA_TYPE:{
+            JSON_ENTRIES_FORMAT: load_json_entries
+        },
+        GPS_TYPE: {
+            JSON_ENTRIES_FORMAT: load_json_entries
+        }
+    }
+
+    def __init__(self, auth, region_name="ap-southeast-2", ddburl="https://dynamodb.ap-southeast-2.amazonaws.com"):
         cclient = boto3.client('cognito-idp', region_name=region_name)
 
         auth_resp = cclient.initiate_auth(
@@ -35,9 +66,47 @@ class Uploader():
         else:
             raise ValueError(f"Could not identify a user sub value in user response: {user_resp}")
 
-    def upload_readings(self, reading_file):
-        pass
-        
+        self.api = API(url=ddburl)
 
-        
+    def load_reading(self, reading):
+        reading_type = reading[self.READING_TYPE_KEY] 
 
+        if reading_type not in self.RECOGNIZED_READING_TYPES:
+            raise ValueError(f"Unrecognize reading type {reading_type} in reading specification {reading}")
+
+        format_type = reading[self.READING_FORMAT_KEY] 
+
+        if format_type not in self.RECOGNIZED_READING_TYPES[reading_type]:
+            raise ValueError(f"Unrecognize format {format_type} for reading of type {reading_type} in reading specification {reading[self.READING_TYPE_KEY]} in reading {reading}")
+
+        return self.RECOGNIZED_READING_TYPES[reading_type][format_type](self.READING_PATH_KEY, reading)
+
+    def generate_route_id(self):
+        return str(datetime.utcnow()) + "_" + ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+
+    def save_entries(self, route_id, entry_type, entries):
+        for i, e in enumerate(entries):
+            self.api.save_entry(
+                route_id,
+                self.usr_sub,
+                i+1,
+                e 
+            )
+
+    def upload(self, route):
+        route_id = self.generate_route_id()
+        print(f"uploading route {route} as {route_id}")
+
+        initial_entries = {}
+
+        for reading in route[self.ROUTE_READINGS_KEY]:
+            print(f"starting upload for reading {reading}")
+
+            entries = self.load_reading(reading)    
+
+            if len(entries) > 0:
+                initial_entries[reading[self.READING_TYPE_KEY]] = entries[0]
+                self.save_entries(route_id, reading[self.READING_TYPE_KEY], entries)
+                print("Finished saving all readings to FDS database")
+            else:
+                print("No entries found for reading specification")
--- upload.py
+++ upload.py
@@ -1,9 +1,10 @@
 import argparse
+import json
 from readingdb.uploader import Uploader
 from readingdb.auth import Auth
 
 parser = argparse.ArgumentParser(description="Uploads given data to the Faultnet Database")
-parser.add_argument("readings", type=str, nargs="+")
+parser.add_argument("routepath", type=str, help="A path to a file containing all the data for a route")
 parser.add_argument("-u", "--username", type=str, help="The AWS cognito login username")
 parser.add_argument("-p", "--password", type=str, help="The AWS cognito login password")
 parser.add_argument("-c", "--clientid", type=str, help="The AWS cognito client id")
@@ -14,4 +15,9 @@ args = parser.parse_args()
 if __name__ == "__main__":
     auth = Auth(args.username, args.password, args.clientid)
 
+    with open(args.routepath, "r") as f:
+        route = json.load(f)
+
     uploader = Uploader(auth)
+
+    uploader.upload(route)
