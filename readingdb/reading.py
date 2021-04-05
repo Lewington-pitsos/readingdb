363f3da23592e7ea52773cbdb181ca139ac6d757
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-04-05 11:21:44 +1000

    add reading colleciton method to readingDB

--- readingdb/reading.py
+++ readingdb/reading.py
@@ -1,4 +1,14 @@
 class Reading():
     # TODO: finalize this class
     # For now see readingdb/test_data/ftg_imgs.json
+    #
+    # A reading is a peice of data collected during a 
+    # route (e.g. images, gps readings, etc) or an 
+    # inference made based on those readings, e.g. 
+    # interplated location based on gps points or
+    # defect location based on images.
+    #
+    # Generally these will be returned in large lists and 
+    # users will filter out readings that theya re not
+    # interested in.
     pass
--- readingdb/readingdb.py
+++ readingdb/readingdb.py
@@ -8,10 +8,10 @@ from readingdb.readingspec import ReadingSpec
 class ReadingDB(abc.ABC):
     # ML API related
     @abc.abstractmethod
-    def route_info(route_id: int) -> Tuple[bool, Route]:
-        """The Boolean indicates whether a route exists for the given id.
+    def route_info(route_id: int) -> Route:
+        """Returns null if no route exists for that route_id.
         """
-        raise NotImplementedError("upload function is not implemented") 
+        raise NotImplementedError("route_info is not implemented") 
 
     @abc.abstractmethod
     def set_as_predicting(route_id: int) -> bool:
@@ -19,7 +19,7 @@ class ReadingDB(abc.ABC):
         that a prediction process is under way). Returns false the route has any status 
         other that 'uploaded' or does not exist.
         """
-        raise NotImplementedError("upload function is not implemented") 
+        raise NotImplementedError("set_as_predicting is not implemented") 
 
     @abc.abstractmethod
     def save_predictions(readings: List[Reading], route_id: int) -> bool:
@@ -28,8 +28,15 @@ class ReadingDB(abc.ABC):
 
         Returns whether the save operation completed sucessfully.
         """
-        raise NotImplementedError("upload function is not implemented")
+        raise NotImplementedError("save_predictions is not implemented")
+
+    @abc.abstractmethod
+    def all_route_readings(route_id: int) -> List[Reading]:
+        """Returns all readings (e.g. gps readings, accelerometer readings, 
+        camera image readings, prior predictions) associated with that route.
+        """
+        raise NotImplementedError("all_route_readings is not implemented")
 
     @abc.abstractmethod
     def upload(reading_spec: ReadingSpec):
-        raise NotImplementedError("upload function is not implemented")
+        raise NotImplementedError("upload is not implemented")
