c21e034a2c073500f58def572707e23f14dfb66d
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-04-06 10:22:14 +1000

    remove out-of-date scripts

--- maketable.py
+++
@@ -1,6 +0,0 @@
-from readingdb.db import DB
-
-if __name__ == '__main__':
-    db = DB("http://localhost:8000")
-    movie_table = db.create_reading_db()
-    print("Table status:", movie_table.table_status)
