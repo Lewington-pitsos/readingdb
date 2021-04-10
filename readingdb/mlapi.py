d097628765f7c2fa878db604424e1442f2f2d21d
SeanMallard <sean@seanmallard.com>
2021-04-10 14:05:21 +1000

    Added a couple of helper methods for sending/receiving/deleting messages to a supplied sqs queue url. There's definitely a better way to handle queue urls, etc. Environment variables, etc. But this will work for now

---
+++ readingdb/mlapi.py
@@ -0,0 +1,68 @@
+import boto3
+from datetime import datetime
+
+from readingdb.sqsqueue import SQSQueue
+
+class MLAPI(SQSQueue):
+
+    sqs = boto3.client('sqs')
+
+
+    def add_message_to_queue(queue_url: str, route_id: str) -> str:
+        response = sqs.send_message(
+            QueueUrl = queue_url,
+            DelaySeconds = 10,
+            MessageAttributes={
+                'RouteID': {
+                    'DataType': 'String',
+                    'StringValue': route_id
+                }
+            },
+            MessageBody=(
+                datetime.now()
+            ) 
+        )
+        return response['MessageId']
+
+
+
+
+
+    def receive_message_from_queue(queue_url: str, ) -> str:
+        response = sqs.receive_message(
+            QueueUrl = queue_url,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMEssages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
+        return response['Messages'][0]
+
+
+
+    def delete_message_from_queue(queue_url: str, message: str) -> None:
+        sqs.delete_message(
+            QueueUrl = queue_url,
+            ReceiptHandle = message['ReceiptHandle']
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
