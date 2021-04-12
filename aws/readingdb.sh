6f81d77645ffede3cdbc3dad8ee9d6aa51fdcd3f
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-04-12 16:53:56 +1000

    update readingdb Dockerfile and move related reference commands to their own file

---
+++ aws/readingdb.sh
@@ -0,0 +1,15 @@
+# deploying to aws ecr
+docker build -f Dockerfile.readingdb -t readingdb:latest ./
+
+aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 950765595897.dkr.ecr.ap-southeast-2.amazonaws.com
+
+# aws ecr create-repository --repository-name readingdb --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
+
+docker tag readingdb:latest 950765595897.dkr.ecr.ap-southeast-2.amazonaws.com/readingdb:latest
+
+docker push 950765595897.dkr.ecr.ap-southeast-2.amazonaws.com/readingdb:latest
+
+# then create a new lambda with the "from image" option
+# ensure to give the lambda proper permissions
+
+# to update the imagem rebuild, retag, push and then select the newer image from the lambda console
