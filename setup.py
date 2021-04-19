80a6c927d8356313cabba1d74902db9b79fdff9d
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-04-19 12:22:39 +1000

    clean up dependencies and add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -17,4 +17,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "boto3",
+        "tqdm",
+        "moto",
+        "botocore",
+    ]
 )
