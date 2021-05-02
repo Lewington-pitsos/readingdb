81e948d95071eba7b96664fbce64cbee67aa56e4
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-05-02 21:12:48 +1000

    create new testing class to handle lambda tests that do not involve reading data

--- readingdb/test_lambda.py
+++ readingdb/test_lambda.py
@@ -121,8 +121,8 @@ class TestSimpleLambdaResponses(TestLambda):
         }, resp)
 
 
-
-class TestDataLambda(TestLambda):
+@mock_s3
+class TestLambdaRW(TestLambda):
     region_name = "ap-southeast-2"
     access_key = "fake_access_key"
     secret_key = "fake_secret_key"
@@ -154,9 +154,47 @@ class TestDataLambda(TestLambda):
         )
     
 
-@mock_s3
-class TestDataLambdaResponses(TestDataLambda): 
+# class TestDataLambda()
 
+class TestLambdaW(TestLambdaRW):
+
+    @unittest.skip("This make an actual call to fargate")
+    def test_correct_upload_event_handling(self):
+        handler({
+             "Type": "NotifyUploadComplete",
+             "AccessToken": "eyJraWQiOiI0QXl3TjdidExEWm1RWFBEdVpxZ3JRTVk2MkVheXc0ZlN6eXBNcFI2bDh3PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5OWJmNDUxOS04NWQ5LTQ3MjYtOTQ3MS00YzkxYTc2Nzc5MjUiLCJjb2duaXRvOmdyb3VwcyI6WyJhZG1pbiJdLCJldmVudF9pZCI6IjljYjMyZjRjLWFhMDktNDk4Yi1hYjkzLTk5ODE3ZjdmNGQxYyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTgyNjc5MzEsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMl9jdHBnbTBLdzQiLCJleHAiOjE2MTgyNzE1MzEsImlhdCI6MTYxODI2NzkzMSwianRpIjoiNmRkNGZiNWMtMTZhZS00N2JjLTg4OTEtODRkYjUzNjg0NmMwIiwiY2xpZW50X2lkIjoiNHVxaHFzb29lNDNlYnRxMG9idm4wbG03dWkiLCJ1c2VybmFtZSI6ImZkc2FkbWluIn0.EgNiuZYENTdIF7t7Zs0LC0UEPaMSc1M66fxfi4OpoLcKvCXdLax6r4wa0gdeL96N6x2PzpBmdxEoeZfSnIFq2NNtcLPXYpmONGgbmP4bxdQW1FcplE6dlvkfo6UnQQdmjTd6r6rTq6CHlHBskFWfi7YRcdbtFf8Ic9nIB2G8J8EkjN1cwGrUUrQ3CqaOuLNjUqxtP6fYgrqEk6lseWVp4P33HK8zOwPUxUuqjwtWfJK_Mchy0QL_K-HpnyUoXU5cv63_PY_OI63QYz5FHFtloTwj1iWqGdE43_tH8AdT3UJpmFNHwHijqVVpsFVmTowRuw1QskdZP-yHK4p7Ea8Y7Q",
+             "Bucket": "mobileappsessions172800-main",
+             "Key": "public/route_2021_04_12_20_59_16_782.zip"
+        }, TEST_CONTEXT)
+
+    @unittest.skipIf(not credentials_present(), NO_CREDS_REASON)
+    def test_accepts_async_readings_request(self):
+        resp = handler({
+            "Type": "GetReadingsAsync",
+            "RouteID": "route-that-doesnt-exist",
+            "AccessToken": self.access_token,
+        }, TEST_CONTEXT)
+
+        self.assertEqual({
+            "Status": "Success",
+            "Body": None
+        }, resp)
+
+    @unittest.skipIf(not credentials_present(), NO_CREDS_REASON)
+    def test_gets_non_existant_route(self):
+        resp = handler({
+            "Type": "GetRoute",
+            "RouteID": "route-that-doesnt-exist",
+            "AccessToken": self.access_token,
+        }, TEST_CONTEXT)
+
+        self.assertEqual({
+            "Status": "Success",
+            "Body": None
+        }, resp)
+
+@mock_s3
+class TestLambdaR(TestLambdaRW): 
     @mock.patch('time.time', mock.MagicMock(side_effect=Increment(1619496879)))
     def setUp(self) -> None:
         super().setUp()
@@ -177,40 +215,18 @@ class TestDataLambdaResponses(TestDataLambda):
         self.twenty_route = r
 
     @unittest.skipIf(not credentials_present(), NO_CREDS_REASON)
-    def test_accepts_async_readings_request(self):
+    def test_gets_routes_for_user(self):
         resp = handler({
-            "Type": "GetReadingsAsync",
-            "RouteID": "route-that-doesnt-exist",
+            "Type": "GetUserRoutes",
             "AccessToken": self.access_token,
         }, TEST_CONTEXT)
 
-        self.assertEqual({
-            "Status": "Success",
-            "Body": None
-        }, resp)
-
-
-    @unittest.skip("This make an actual call to fargate")
-    def test_correct_upload_event_handling(self):
-        handler({
-             "Type": "NotifyUploadComplete",
-             "AccessToken": "eyJraWQiOiI0QXl3TjdidExEWm1RWFBEdVpxZ3JRTVk2MkVheXc0ZlN6eXBNcFI2bDh3PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5OWJmNDUxOS04NWQ5LTQ3MjYtOTQ3MS00YzkxYTc2Nzc5MjUiLCJjb2duaXRvOmdyb3VwcyI6WyJhZG1pbiJdLCJldmVudF9pZCI6IjljYjMyZjRjLWFhMDktNDk4Yi1hYjkzLTk5ODE3ZjdmNGQxYyIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MTgyNjc5MzEsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMl9jdHBnbTBLdzQiLCJleHAiOjE2MTgyNzE1MzEsImlhdCI6MTYxODI2NzkzMSwianRpIjoiNmRkNGZiNWMtMTZhZS00N2JjLTg4OTEtODRkYjUzNjg0NmMwIiwiY2xpZW50X2lkIjoiNHVxaHFzb29lNDNlYnRxMG9idm4wbG03dWkiLCJ1c2VybmFtZSI6ImZkc2FkbWluIn0.EgNiuZYENTdIF7t7Zs0LC0UEPaMSc1M66fxfi4OpoLcKvCXdLax6r4wa0gdeL96N6x2PzpBmdxEoeZfSnIFq2NNtcLPXYpmONGgbmP4bxdQW1FcplE6dlvkfo6UnQQdmjTd6r6rTq6CHlHBskFWfi7YRcdbtFf8Ic9nIB2G8J8EkjN1cwGrUUrQ3CqaOuLNjUqxtP6fYgrqEk6lseWVp4P33HK8zOwPUxUuqjwtWfJK_Mchy0QL_K-HpnyUoXU5cv63_PY_OI63QYz5FHFtloTwj1iWqGdE43_tH8AdT3UJpmFNHwHijqVVpsFVmTowRuw1QskdZP-yHK4p7Ea8Y7Q",
-             "Bucket": "mobileappsessions172800-main",
-             "Key": "public/route_2021_04_12_20_59_16_782.zip"
-        }, TEST_CONTEXT)
-
-    @unittest.skipIf(not credentials_present(), NO_CREDS_REASON)
-    def test_gets_non_existant_route(self):
-        resp = handler({
-            "Type": "GetRoute",
-            "RouteID": "route-that-doesnt-exist",
-            "AccessToken": self.access_token,
-        }, TEST_CONTEXT)
-
-        self.assertEqual({
-            "Status": "Success",
-            "Body": None
-        }, resp)
+        self.assertEqual(len(resp["Body"]), 3)
+        self.assertTrue(resp["Body"][0]["RouteID"] in [
+            self.twenty_route.id,
+            self.gps_img_route.id,
+            self.tst_route.id
+        ])
 
     @unittest.skipIf(not credentials_present(), NO_CREDS_REASON)
     def test_gets_readings(self):
@@ -237,20 +253,6 @@ class TestDataLambdaResponses(TestDataLambda):
     #     self.assertEqual(resp["Body"]['Key'], "rulerruler.json")
 
     @unittest.skipIf(not credentials_present(), NO_CREDS_REASON)
-    def test_gets_routes_for_user(self):
-        resp = handler({
-            "Type": "GetUserRoutes",
-            "AccessToken": self.access_token,
-        }, TEST_CONTEXT)
-
-        self.assertEqual(len(resp["Body"]), 3)
-        self.assertTrue(resp["Body"][0]["RouteID"] in [
-            self.twenty_route.id,
-            self.gps_img_route.id,
-            self.tst_route.id
-        ])
-
-    @unittest.skipIf(not credentials_present(), NO_CREDS_REASON)
     def test_gets_route(self):
         resp = handler({
             "Type": "GetRoute",
@@ -262,10 +264,12 @@ class TestDataLambdaResponses(TestDataLambda):
         del resp["Body"]["SampleData"]["PredictionReading"]["Reading"]["PresignedURL"]
 
         self.maxDiff = None
+
+        self.assertIn("LastUpdated", resp["Body"])
+        del resp["Body"]["LastUpdated"]
         self.assertEqual({
             'Status': 'Success', 
             'Body': {
-                'LastUpdated': 1619496890,
                 'RouteStatus': 1, 
                 'Timestamp': 1616116106935,
                 'UserID': '99bf4519-85d9-4726-9471-4c91a7677925', 
