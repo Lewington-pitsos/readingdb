d27bf52a9f7c06619763e1d16e1d76d52992e94e
Louka Ewington Pitsos <lewingtonpitsos@gmail.com>
2021-06-09 08:13:51 +1000

    add some decent test data for testing geolocation code

--- readingdb/test_geolocator.py
+++ readingdb/test_geolocator.py
@@ -1,18 +1,23 @@
+import json
 from readingdb.geolocator import Geolocator
 import unittest
 from readingdb.format import *
 
 class TestGeolocator(unittest.TestCase):
-    def test_snapping(self):
-        pos_readings = []
-        snapped_pos_readings = Geolocator.geolocate(pos_readings)
+    @classmethod
+    def setUpClass(cls) -> None:
+        with open('readingdb/test_data/melb_gps_img.json', 'r') as f:
+            allReadings = json.load(f)
+        
+        cls.pos_readings = [r for r in allReadings if r[ReadingKeys.TYPE] == ReadingTypes.POSITIONAL] 
+        cls.img_readings = [r for r in allReadings if r[ReadingKeys.TYPE] == ReadingTypes.IMAGE] 
 
-        self.assertEqual(len(pos_readings), len(snapped_pos_readings))
+    def test_snapping(self):
+        snapped_pos_readings = Geolocator.geolocate(self.pos_readings)
+
+        self.assertEqual(len(self.pos_readings), len(snapped_pos_readings))
 
     def test_prediction_generating(self):
-        pos_readings = []
-        img_readings = []
+        predictions = Geolocator.generate_predictions(self.pos_readings, self.img_readings)
 
-        predictions = Geolocator.generate_predictions(pos_readings, img_readings)
-
-        self.assertEqual(len(img_readings), len(predictions))
+        self.assertEqual(len(self.img_readings), len(predictions))
